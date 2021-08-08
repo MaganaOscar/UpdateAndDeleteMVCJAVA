97290ed3d4b025379dc438198a64502dedc58e5b
MaganaOscar <oscmag13@gmail.com>
2021-08-07 19:41:54 -0700

    asgt complete

---
+++ src/main/java/com/oscar/mvc/repositories/BookRepository.java
@@ -0,0 +1,20 @@
+package com.oscar.mvc.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.oscar.mvc.models.Book;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+	// this method retrieves all the books from the database
+    List<Book> findAll();
+    // this method finds books with descriptions containing the search string
+    List<Book> findByDescriptionContaining(String search);
+    // this method counts how many titles contain a certain string
+    Long countByTitleContaining(String search);
+    // this method deletes a book that starts with a specific title
+    Long deleteByTitleStartingWith(String search);
+    
+//    void deleteById(Long id);
+}
